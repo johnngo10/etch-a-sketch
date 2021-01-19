cce209205514cd82ead5dc57b6bfd047fb2707ea
xuanngo10 <xuan.ngo10@gmail.com>
2021-01-18 22:00:34 -0800

    added mouseover to each square

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const display = document.getElementById('display');
 const submit = document.getElementById('submit');
+const inputContainer = document.getElementById('input-container');
 
+// Create Grid
 function createGrids() {
   const square = document.createElement('div');
   square.classList.add('squares');
+  square.addEventListener('mouseover', e => {
+    square.style.backgroundColor = 'black';
+  });
   display.appendChild(square);
 }
 
+// Load default 16 x 16 grid
 function loadDefault() {
   let numOfSq = 16 * 16;
 
@@ -17,16 +23,40 @@ function loadDefault() {
 
 loadDefault();
 
+// Check for valid number
+function numValidation() {}
+
+// Display new grid based on user's input
 submit.addEventListener('click', e => {
   e.preventDefault();
   let num = document.getElementById('numInput').value;
   let calc = num * num;
 
-  display.innerHTML = '';
-  display.style.gridTemplateColumns = `repeat(${num}, 2fr)`;
-  display.style.gridTemplateRows = `repeat(${num}, 2fr)`;
+  let regex = /^\d+$/;
 
-  for (let i = 0; i < calc; i++) {
-    createGrids();
+  if (num.match(regex) && num <= 64) {
+    display.innerHTML = '';
+    display.style.gridTemplateColumns = `repeat(${num}, 2fr)`;
+    display.style.gridTemplateRows = `repeat(${num}, 2fr)`;
+    const error = document.getElementById('error');
+
+    // Remove any existing error messages
+    if (error) {
+      error.remove();
+    }
+
+    for (let i = 0; i < calc; i++) {
+      createGrids();
+    }
+  } else {
+    const errorEle = document.createElement('p');
+    errorEle.textContent = '* Must be a number between 1-64';
+    errorEle.setAttribute('id', 'error');
+    const error = document.getElementById('error');
+
+    // Create error message if one hasn't existed
+    if (!error) {
+      inputContainer.appendChild(errorEle);
+    }
   }
 });
